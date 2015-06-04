01beae5f3ec47a0be35fafcb8b3fb1f952940884
mehrdadrafiee <rafiee.mehrdad@gmail.com>
2015-06-04 10:32:22 -0500

    Used "yo angular:route routeName" to create the "Contact" view.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,6 +33,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoDoApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the yoDoApp
+ */
+angular.module('yoDoApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
